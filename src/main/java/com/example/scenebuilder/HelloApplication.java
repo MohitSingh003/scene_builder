7394e31858da5038c844f762ea9536d0b6cf6549
Mohit Singh Sahare <saharemr@rknec.edu>
2023-12-02 20:26:19 +0530

    Started with scene builder

---
+++ src/main/java/com/example/scenebuilder/HelloApplication.java
@@ -0,0 +1,23 @@
+package com.example.scenebuilder;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class HelloApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("app.fxml"));
+        stage.setTitle("First sceneBuilder Program");
+        stage.setScene(new Scene(root));
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
